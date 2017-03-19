6825cd056c3010c79a6083bf3d6e3f10a0d9dbe5
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-03-19 23:27:50 +0000

    Add GNUmakefile

---
+++ GNUmakefile
@@ -0,0 +1,2 @@
+all:
+	g++ -pedantic -Wall -Wextra -std=c++1z
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,34 @@
 #include <array>
 #include <tuple>
 
+using std::begin;
+using std::end;
+
+template <typename A, typename B> auto fmap(std::deque<A> fa, std::function<B(A)> f) -> std::deque<B>
+{
+    std::deque<B> fb { fa.size(), typename std::deque<B>::allocator_type { } };
+
+    std::transform(begin(fa), end(fa), begin(fb), f);
+
+    return fb;
+}
+
+template <typename T> using Seq = std::deque<T>;
+
 int main(int, char**) {
+
+    auto wat = Seq<int> { 1, 2, 3, 4, 5, 6 };
+
+    auto watt = fmap(wat, std::function<auto (int) -> std::future<int>> { [] (auto i) {
+        std::promise<int> p;
+        p.set_value(i);
+        return p.get_future();
+    }});
+
+    for (auto& omj: watt) {
+        omj.wait();
+        std::cout << omj.get() << "\n";
+    }
+
     return 0;
 }
